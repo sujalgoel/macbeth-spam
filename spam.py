a6f6df1b9981028546c158c56c7da467978fe176
Sujal Goel <gglebot.helper@gmail.com>
2021-10-11 16:23:20 +0530

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,30 +1,14 @@
-import pyautogui
 import time
-import os
+import pyautogui
 
-os.system("echo //////////////////////////////////////////////////////////////////////////////////////////")
-os.system("echo ///   ::::::::::     :::      :::      ::::::::::::      ::::::::::::      :::         ///")
-os.system("echo ///   :::            :::      :::           :::          :::      :::      :::         ///")
-os.system("echo ///   :::            :::      :::           :::          :::      :::      :::         ///")
-os.system("echo ///   :::::::::      :::      :::           :::          :::      :::      :::         ///")
-os.system("echo ///         :::      :::      :::           :::          ::::::::::::      :::         ///")
-os.system("echo ///         :::      :::      :::           :::          :::      :::      :::         ///")
-os.system("echo ///   :::::::::      ::::::::::::      ::::::::          :::      :::      :::::::::   ///")
-os.system("echo //////////////////////////////////////////////////////////////////////////////////////////")
-os.system("echo **CREATED THIS MACBETH SPAM!**")
-os.system("echo Find some other code here!      https://sujalgoel.ml")
+print("\nFind some stuff here: https://sujalgoel.engineer\n")
 
-seconds=10
-
-for i in range(seconds):
-    print(str(seconds - i) + " seconds remaining to start the script")
+for i in range(10):
+    print(f"{str(10 - i)} seconds remaining to start the script.")
     time.sleep(1)
 
 time.sleep(1)
-file = open("macbeth.txt", "r")
 
-for word in file:
+for word in open("macbeth.txt"):
     pyautogui.typewrite(word)
     pyautogui.press("enter")
-
-input("Press enter and exit")
